5f5b61c7ea843665fda57304c5e28030605dd859
Will Hilton <wmhilton@gmail.com>
2017-11-19 00:01:19 -0500

    savepoint

--- bin/modload.js
+++ bin/modload.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+const builtins = require('builtin-modules')
 let Module = require('module')
 const fs = require('fs')
 const upath = require('upath')
@@ -7,13 +8,10 @@ const sortKeys = require('sort-keys')
 const shasum = require('shasum')
 const util = require('util')
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
-
 // Remove the modrun.js argument, so the actual main module isn't confused when it parses argv.
 process.argv.splice(1, 1)
-
 // For easier reading of output, lets shave some path info.
 let rootDir = upath.dirname(upath.resolve(process.argv[1]))
-
 let paths = {}
 let shas = {}
 let files = {}
@@ -28,11 +26,46 @@ if (fs.existsSync(indexCacheFilepath)) {
 }
 
 // Now we need to modify the semantics of 'require'
-let requireHacker = require('require-hacker')
+// Notes:
+// Module.prototype.require is only three lines of code. Plus tricky to alter permanently.
+// The better place to tinker is Module._load. It has the perfect function signature for
+// total control: (requiredPath, parentModule, isMain) -> module.exports
+const __cache = {}
+Module._cache = new Proxy(__cache, {
+  set (target, property, value, receiver) {
+    target[property] = value
+    return true
+  },
+  get (target, property, receiver) {
+    return target[property]
+  }
+})
+
+const __compile = module._compile.bind(module)
+const __load = Module._load.bind(Module)
+Module._load = function _load (path, module, isMain = false) {
+  let jawn = modload(path, module, isMain)
+  if (jawn === undefined) {
+    return __load(path, module, isMain)
+  } else {
+    let foo = __compile(jawn.source, jawn.path)
+    console.log(foo)
+    Module._cache[jawn.path] = foo
+    if (isMain) {
+      process.mainModule = foo
+    }
+    return foo.exports
+  }
+}
+
 let alreadyLoaded = new Set()
-requireHacker.global_hook('modload', (path, module) => {
+function modload (path, module, isMain) {
+  // Ignore builtins
+  if (builtins.includes(path)) return
+  // start
   const start = (module === null || module.id === '.') ? '.' : upath.normalizeTrim(upath.joinSafe('./', upath.relative(rootDir, module && module.filename && module.filename.replace(/\.modload/, '') || '')))
-  const result = (paths && paths[start] && paths[start][path]) || upath.normalizeTrim(upath.joinSafe('./', upath.relative(rootDir, requireHacker.resolve(path, module))))
+  console.log('RESOLVING', start, path)
+  const result = (paths && paths[start] && paths[start][path]) || upath.normalizeTrim(upath.joinSafe('./', upath.relative(rootDir, Module._resolveFilename(path, module, isMain))))
   // add to graph
   paths[start] = paths[start] || {}
   paths[start][path] = result
@@ -68,7 +101,7 @@ requireHacker.global_hook('modload', (path, module) => {
     files[sha] = source
   })
   return
-})
+}
 
 process.on('exit', function () {
   let cache = JSON.stringify(sortKeys({
@@ -78,7 +111,6 @@ process.on('exit', function () {
   }, {deep: true}), null, 2)
   fs.writeFileSync(indexCacheFilepath, cache, 'utf8')
 })
-
 // Finally, we need to convince this module he's the main module.
 // Unfortunately, this is not part of the documented API.
 // https://github.com/nodejs/node/blob/master/lib/module.js#L602
